8ad9584cd469ed66fbd7a363dee4e91f57c684eb
Mubanga <mubangeriseme@yahoo.com>
2019-06-11 20:30:14 +0200

    Final Portrait Layout Complete

--- app/src/main/java/com/learnprogramming/academy/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/calculatorapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.learnprogramming.academy.calculatorapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var CalculatorScreen:EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CalculatorScreen = edtText_Calculation_Screen
+//        CalculatorScreen?.keyListener = null
+/* CalculatorScreen?.keyListener = null */
     }
 }
