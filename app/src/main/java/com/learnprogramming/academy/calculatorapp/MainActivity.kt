c24e92dccff2e50fb1b7af4288efdcee0250462c
Mubanga <mubangeriseme@yahoo.com>
2019-06-12 03:26:18 +0200

    Completed onClickListners for Clear and NumButtons
    TODO: Complete onClickListner for Equals

--- app/src/main/java/com/learnprogramming/academy/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/learnprogramming/academy/calculatorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.learnprogramming.academy.calculatorapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,14 +17,65 @@ class MainActivity : AppCompatActivity() {
     private var operand2:Double = 0.0
     private var pendingOperation = ""
     private lateinit var NumButtons:ArrayList<Button> // Numerical Number Buttons
-    private lateinit var button1:Button
+    private lateinit var OperationButtons:ArrayList<Button> // Operation Buttons
+    private val CurrentOperations = arrayListOf<String>() // ArrayList That Stores The Operations That Need To Be Performed
+    private var OperationsMap = mapOf("Cancel" to "C","Division" to "/","Multiplication" to "x","Delete" to "DEL","Minus" to "-","Add" to "+","Brackets" to "()","Equals" to "=")
+    //OperationsMap = mapOf("Cancel" to "C","Division" to "/","Multiplication" to "x","Delete" to "DEL","Minus" to "-","Add" to "+","Brackets" to "()","Equals" to "=")
+
+
+    private fun OperationsOnClickListner(Operation : String) : View.OnClickListener
+    {
+        //OperationsMap.get(Operation)
+        val CurrentOnClickListner = View.OnClickListener { v->
+            val OperationButton = v as Button
+            var ButtonText:String = Operation
+            if(OperationButton.text.toString()!=null)
+            {
+                ButtonText = OperationButton.text.toString()
+            }
+            //val ButtonText:String = OperationsMap.get(OperationButton.text.toString())
+            if(ButtonText!="C"||ButtonText!="DEL"||ButtonText!="=")
+            {
+
+            }
+            else
+            {
+                //TODO: Implement Delete and Equals OnClickListners
+                if(ButtonText == "C")
+                {
+                        CalculatorScreen.text.clear()
+                }
+                if(ButtonText == "DEL")
+                {
+                    CalculatorScreen.text.delete(CalculatorScreen.text.lastIndex-1,CalculatorScreen.text.lastIndex)
+                }
+            }
+        }
+//        if(Operation!="Cancel"||Operation!="Delete"||Operation!="Equals")
+//        {
+//            if(Operation == "Cancel")
+//            {
+//                val ClearOnclickListner = View.OnClickListener { v ->
+//                    CalculatorScreen.text.clear()
+//                }
+//            }
+//
+//        }
+//        else
+//        {
+//            CurrentOperations.add(Operation)
+//            CalculatorScreen.append(OperationsMap.get(Operation))
+//        }
+        return CurrentOnClickListner
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         CalculatorScreen = edtText_Calculation_Screen
-
-        // Numpad Button Declarations
+        //OperationsMap = mapOf("Cancel" to "C","Division" to "/","Multiplication" to "x","Delete" to "DEL","Minus" to "-","Add" to "+","Brackets" to "()","Equals" to "=")
+        //********** Numpad Button Declarations ***************
         NumButtons = ArrayList<Button>()
         for(x in 0..9)
         {
@@ -33,9 +85,54 @@ class MainActivity : AppCompatActivity() {
             val Num_Button = findViewById<Button>(Resource_ID)
 
             // Establish onClickListners For All The Numpad Buttons
+            val NumPadListner = View.OnClickListener { v ->
+                val numButton = v as Button // When the button is clicked a reference will be returned that needs to be casted as a button
+                CalculatorScreen.append(numButton.text) // Display The Newly Added Number In The CalculatorScreen
+            }
+            Num_Button.setOnClickListener(NumPadListner)
             NumButtons.add(Num_Button)
-
         }
+
+        // ************ OPERATION OnClickListners ******************
+        val OperationClearListner = View.OnClickListener { v ->
+            val operationButton = v as Button
+            CalculatorScreen.text.clear() // Clear Calculator Screen And All Pending Operation Interactions
+            // TODO: Clear Pending Operand Terms And Operations *****
+        }
+
+        // ***** ADDITION *******
+        val OperationAddListner = View.OnClickListener { v ->
+            val operationButton = v as Button
+            CalculatorScreen.append("+")
+        }
+        // ***** DIVISION *******
+        val OperationDivisionListner = View.OnClickListener { v ->
+            val operationButton = v as Button
+            CalculatorScreen.append("/")
+        }
+
+//        // ***** DIVISION *******
+//        val OperationDivisionListner = View.OnClickListener { v ->
+//            val operationButton = v as Button
+//            CalculatorScreen.append("/")
+//        }
+
+
+        // ************ OPERATION Button Declarations ***************
+        OperationButtons = ArrayList<Button>()
+        val PrefixButtonOperations = listOf<String>("Cancel","Division","Multiplication","Delete","Minus","Add","Brackets","Equals")
+        for(x in PrefixButtonOperations)
+        {
+            val Prefix_Button_Operation = "btn_Operation_"
+            val Operations_Button_ID = Prefix_Button_Operation + x
+            val Resource_ID = resources.getIdentifier(Operations_Button_ID,"id",packageName)
+            val operationButton = findViewById<Button>(Resource_ID)
+            operationButton.setOnClickListener(OperationsOnClickListner(operationButton.text.toString()))
+            OperationButtons.add(operationButton)
+        }
+
+
+
 //        CalculatorScreen?.keyListener = null
 /* CalculatorScreen?.keyListener = null */
     }
